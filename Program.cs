f735aad064eacc00d49b0f4a5b3cbec04c957c9f
Christian Tschenett <developer@furthermore.ch>
2020-03-02 22:21:51 +0100

    local SQS, docker and AWS broken

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,9 +11,66 @@ using Serilog;
 using Serilog.Events;
 //using Serilog.Formatting.Compact;
 using Serilog.Formatting.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
 namespace TodoApi
 {
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IAmazonSQS _sqs;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IAmazonSQS sqs, IConfiguration configuration) 
+        {
+            _logger = logger;
+            _sqs = sqs;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested) 
+            {
+                _logger.LogDebug("Long-polling SQS messages...");
+
+                try 
+                {
+                    var request = new ReceiveMessageRequest()
+                    {
+                        QueueUrl = _configuration.GetSection("queues")["main"],
+                        WaitTimeSeconds = 20
+                    };
+                    var result = await _sqs.ReceiveMessageAsync(request, stoppingToken);
+                    if (result.Messages.Any())
+                    {
+                        foreach (var message in result.Messages)
+                        {
+                            _logger.LogInformation("SQS message received: {msg}", message.Body);
+
+                            await _sqs.DeleteMessageAsync(new DeleteMessageRequest() { 
+                                QueueUrl = request.QueueUrl,
+                                ReceiptHandle = message.ReceiptHandle
+                            }, stoppingToken);
+                        }
+                    }
+                }
+                catch (System.UriFormatException e) 
+                {
+                    throw e; //aborts app with ... "Level":"Fatal","MessageTemplate":"Application start-up failed","Exception":"System.UriFormatException: Invalid URI:  ...
+                }
+                catch (Exception e) 
+                {
+                    _logger.LogError("SQS polling- or message processing issue (re-trying in 10s): {exception}", e); //FIXME no blind retry in prod ;-)
+
+                    await Task.Delay(10000, stoppingToken);
+                }
+            }
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -42,6 +100,11 @@ namespace TodoApi
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseSerilog()
+                .ConfigureServices(services =>
+                {
+                    services.AddAWSService<IAmazonSQS>();
+                    services.AddHostedService<Worker>();                    
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
