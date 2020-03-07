86d100bd61c4aa235228f44da50f8848f5d4a01f
Christian Tschenett <developer@furthermore.ch>
2020-03-07 12:45:48 +0100

    minor tweaks

--- create_stack_acm.sh
+++ create_stack_acm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Pre-cond: CloudTrail must be enabled!
-
 aws cloudformation create-stack --stack-name acm-wildcard --template-body file://./cf/acm.yaml
 
 aws cloudformation wait stack-create-complete --stack-name acm-wildcard
